1ac0575678ef4eab7c05b121003def084459daa1
HanifSauqi <126406854+HanifSauqi@users.noreply.github.com>
2023-02-27 07:11:04 +0700

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,48 +1,31 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Assigment1 {
+public class Tugas {
     public static void main(String[] args) {
-        // Daftar barang yang akan dibeli
-        String[] daftarBarang = {"cabai", "bayam", "bawang", "tempe", "beras"};
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Cabai");
+        cars.add("Bayam");
+        cars.add("Bawang");
+        cars.add("Tempe");
+        cars.add("Beras");
+        System.out.println("Daftar Catatan Dina : "+ cars);
 
-        // Harga masing-masing barang
-        int[] harga = {3000, 2000, 3000, 5000, 20000};
+        cars.add("Ayam");
+        cars.add("Minyak");
+        cars.remove("Cabai");
+        System.out.println("Barang yang dibeli Dina  : "+ cars);
 
-        // Uang yang dibawa Dina
         int uangDina = 50000;
 
-        // Membuat ArrayList untuk menyimpan barang yang dibeli Dina
-        ArrayList<String> barangDibeli = new ArrayList<>();
+        int[] harga = {2000, 3000, 5000, 20000, 20000, 12000};
 
-        // Menghitung total belanja Dina
         int totalBelanja = 0;
-        for (int i = 0; i < daftarBarang.length; i++) {
+        for (int i = 0; i < cars.size(); i++) {
             // Jika barang tersebut tidak dibeli Dina
-            if (daftarBarang[i].equals("cabai")) {
-                continue;
-            }
-
-            // Jika barang tersebut dibeli Dina
             int hargaBarang = harga[i];
             totalBelanja += hargaBarang;
-
-
-            barangDibeli.add(daftarBarang[i]);
         }
-
-        // Menambahkan barang baru yang dibeli Dina
-        barangDibeli.add("ayam");
-        barangDibeli.add("minyak");
-        totalBelanja += harga[4] + harga[2];
-
-        // Menampilkan daftar catatan Dina
-        System.out.println("Daftar catatan Dina: " + Arrays.toString(daftarBarang));
-
-        // Menampilkan barang yang dibeli Dina
-        System.out.println("Barang yang dibeli Dina: " + barangDibeli);
-
-        // Menampilkan total belanja Dina serta keterangan kurang, pas, atau lebih
         System.out.println("Total belanja Dina: " + totalBelanja);
         if (totalBelanja < uangDina) {
             System.out.println("Kurang " + (uangDina - totalBelanja));
